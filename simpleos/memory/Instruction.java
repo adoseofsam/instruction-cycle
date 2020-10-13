d5b664a301289a4027f1aa456c46397736b1c845
adoseofsam <jamessamantha099@gmail.com>
2020-10-13 18:04:25 -0500

    changes to processor class

--- simpleos/memory/Instruction.java
+++ simpleos/memory/Instruction.java
@@ -3,6 +3,8 @@ package simpleos.memory;
 public class Instruction{
     private int instruction_bits;
     private int address_bits;
+    private static final int maxcode = 2;
+    private MyMemory memory;
 
 
     public Instruction(int instruction_bits, int address_bits){
@@ -14,12 +16,25 @@ public class Instruction{
 
     }
 
-    public int getInstruction(){
-        return this.instruction_bits;
+    public int getInstruction(int value){
+        int totalint = 1 + (int) Math.log10(value);
+        if (totalint <= maxcode ) return 0;
+        int expo = totalint - maxcode;
+        int power = (int) Math.pow(10, expo);
+        int operationcode = value/ power;
+        int op = memory.toBinary(operationcode);
+        return op;
+        // return this.instruction_bits;
     }
 
-    public int getAddress(){
-        return this.address_bits;
+    public int getAddress(int value){
+        int totalint = 1 + (int) Math.log10(value);
+        if (totalint <= maxcode) return value;
+        int expo = totalint - maxcode;
+        int power = (int) Math.pow(10, expo);
+        int addr = value % power;
+        memory.toBinary(addr);
+        return addr;
     }
 
     
